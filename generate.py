68d6c9ebd4fd52b5714014dd248463d5aab0eb2d
armansw <37415995+armansw@users.noreply.github.com>
2018-03-19 12:08:34 +0300

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,33 @@
+from train import train
+from random import uniform
+
+def unirand(seq):
+    sum_, freq_ = 0, 0
+    for item, freq in seq:
+        sum_ += freq
+    rnd = uniform(0, sum_)
+    for token, freq in seq:
+        freq_ += freq
+        if rnd < freq_:
+            return token
+
+
+
+def generate_sentence(model):
+    phrase = ''
+    t0, t1 = '$', '$'
+    while 1:
+        t0, t1 = t1, unirand(model[t0, t1])
+        if t1 == '$': break
+        if t1 in ('.!?,;:') or t0 == '$':
+            phrase += t1
+        else:
+            phrase += ' ' + t1
+    return phrase.capitalize()
+
+
+
+if __name__ == '__main__':
+    model = train(open('tolstoy.txt', 'r'))
+    for i in range(10):
+        print(generate_sentence(model))
