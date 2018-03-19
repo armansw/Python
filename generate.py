b6d1132ac168cd9e2f08a28a6fa3ee9ce005c714
armansw <37415995+armansw@users.noreply.github.com>
2018-03-20 01:18:50 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,5 +1,8 @@
 from train import train
-from random import uniform
+import argparse
+import sys
+import random
+import pickle
 
 
 def unirand(
@@ -7,27 +10,55 @@ def unirand(
     sum_, freq_ = 0, 0
     for item, freq in seq:
         sum_ += freq
-    rnd = uniform(0, sum_)
+    rnd = random.uniform(0, sum_)
     for token, freq in seq:
         freq_ += freq
         if rnd < freq_:
             return token
 
 
-def generate_sentence(model):  # Генерирует модель
+def generate_sentence(model, t0, length):  # Генерирует модель
     phrase = ''
-    t0, t1 = '$', '$'
-    while 1:
+    t1 = '$'
+    if length == 0:
+        length = random.randint(1, 10)
+    for _ in range(length):
         t0, t1 = t1, unirand(model[t0, t1])
         if t1 == '$': break
         if t1 in ('.!?,;:') or t0 == '$':
             phrase += t1
         else:
             phrase += ' ' + t1
-    return phrase.capitalize()
+    return phrase.capitalize() + '\n'
 
 
-if __name__ == '__main__':
-    model = train(open('tolstoy.txt', 'r'))
-    for i in range(10):
-        print(generate_sentence(model))
+if __name__ == '__main__':  # Консольный интерфейс приложения
+    parser = argparse.ArgumentParser(description='Generate text model.')
+    parser.add_argument(  # Команда ввода начального слова
+        '--seed',
+        dest='seed',
+        type=str,
+        default="$",
+        help='starting word (default: random)')
+    parser.add_argument(  # Команда ввода файла
+        '--output',
+        dest='output',
+        type=argparse.FileType('w'),
+        default=sys.stdout,
+        help='output model (default: stdout)')
+    requiredNamed = parser.add_argument_group('required arguments')
+    requiredNamed.add_argument(  # Команда ввода модели
+        '--model',
+        dest='model',
+        type=argparse.FileType('rb'),
+        help='input model',
+        required=True)
+    requiredNamed.add_argument(  # Команда ввода длины сгенерированного текста
+        '--length',
+        dest='length',
+        type=int,
+        help='length of the sequence to be generated',
+        required=True)
+    args = parser.parse_args()
+    args.output.write(
+        generate_sentence(pickle.load(args.model), args.seed, args.length))
